8924c7df959ba52db26f391bcc524a22512c4b4f
Ian A. Mason <iam@csl.sri.com>
2005-04-13 08:18:29 +0000

    Tried to fix the macrostr.c file, its in the c_makefile now, so just a pain make will test it, also there is a comment in the c_makefile to show where to comment it out.

--- src/macrostr.c
+++ src/macrostr.c
@@ -22,7 +22,7 @@
 #include "ec.h"
 #include "macrostr.h"
 
-#ifndef DARWIN
+#ifndef _MAC
 #include <sys/msg.h>
 #endif
 #include <sys/sem.h>
@@ -30,27 +30,25 @@
 #include <sys/mman.h>
 
 static struct {
-	char *ms_cat;
-	intptr_t ms_code;
-	char *ms_macro;
-	char *ms_desc;
+  char *ms_cat;
+  intptr_t ms_code;
+  char *ms_macro;
+  char *ms_desc;
 } macrostr_db[] = {
 #include "macrostr.incl"
-	{ NULL, 0, NULL, NULL}
+  { NULL, 0, NULL, NULL}
 };
 
-char *get_macrostr(const char *cat, int code, char **desc)
-{
-	int i;
-
-	for (i = 0; macrostr_db[i].ms_cat != NULL; i++)
-		if (strcmp(macrostr_db[i].ms_cat, cat) == 0 &&
-		  macrostr_db[i].ms_code == code) {
-			if (desc != NULL)
-				*desc = macrostr_db[i].ms_desc;
-			return macrostr_db[i].ms_macro;
-		}
-	if (desc != NULL)
-		*desc = "?";
-	return "?";
+char *get_macrostr(const char *cat, int code, char **desc){
+  int i;
+  for (i = 0; macrostr_db[i].ms_cat != NULL; i++)
+    if (strcmp(macrostr_db[i].ms_cat, cat) == 0 &&
+	macrostr_db[i].ms_code == code) {
+      if (desc != NULL)
+	*desc = macrostr_db[i].ms_desc;
+      return macrostr_db[i].ms_macro;
+    }
+  if (desc != NULL)
+    *desc = "?";
+  return "?";
 }
