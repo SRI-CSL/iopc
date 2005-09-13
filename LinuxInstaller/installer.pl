60ff43766a2aeec50f85c68fabd0d62472a39e2d
Ian A. Mason <iam@csl.sri.com>
2005-09-13 19:57:10 +0000

    Fixed the ~ problem?

--- LinuxInstaller/installer.pl
+++ LinuxInstaller/installer.pl
@@ -7,25 +7,41 @@ my $FMODE = 0777;
 
 my $zipfile = <IOP*.zip>;
 
-my $iopbindir = undef;
+my $iopbindir = $ENV{IOPBINDIR};
+
+my $tilde = getTilde();
 
 if(!$zipfile){
     die "Sorry but I can't find my zip file!\n";
 }
 
-print <<END_OF_INTRO;
+if(defined $iopbindir){
+print <<END_OF_INTROA;
 Welcome to the IOP installer. 
 
+You have set the environment variable 
+
+IOPBINDIR to ${iopbindir}
+
+so I will use that directory.
+
+END_OF_INTROA
+
+} else {
+
+print <<END_OF_INTROB;
+Welcome to the IOP installer. 
+	
 To install IOP you will need to choose a directory to hold the
 binaries. This directory should then be added to your path. Typical
 choices are:
-
-    /usr/local/iop
-    ~/bin/iop
-
+	
+/usr/local/iop
+~/bin/iop
+	
 To do the former you may need to run this script as root.
 
-END_OF_INTRO
+END_OF_INTROB
 
 while(!$iopbindir){
     print "Please enter the iop bin directory location: ";
@@ -33,7 +49,11 @@ while(!$iopbindir){
     chomp($iopbindir);
 }
 
+${iopbindir} =~ s/~/$tilde/;
+
 print "You chose ${iopbindir} as the iop bin directory location.\n";
+    
+}
 
 if(-e $iopbindir){
     print "This location appears to already exist.\n";
@@ -46,3 +66,9 @@ if(-e $iopbindir){
 }
 
 print `unzip -d $iopbindir $zipfile`;
+
+
+sub getTilde {
+    my @my_pw_entry  =  getpwuid($<);
+    return $my_pw_entry[7];
+}
