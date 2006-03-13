1c9fedbff5e358f74df102baa35d80142da1d31f
Ian A. Mason <iam@csl.sri.com>
2006-03-13 22:51:36 +0000

    Turned off the Maude wrapper logging!

--- src/dbugflags.h
+++ src/dbugflags.h
@@ -22,7 +22,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#define WATCH_MAUDE               1
+#define WATCH_MAUDE               0
 #define MAUDE_LOGFILE             "/tmp/maude-out.txt"
 
 #define CHATTER                   0
