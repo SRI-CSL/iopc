9f3e54f03c29ccec12506546c77b541b02df504a
Ian A. Mason <iam@csl.sri.com>
2013-02-15 22:17:39 +0000

    Reorg for move.

---
+++ Antlr4/src/dotparser/visitor/Main.java
@@ -0,0 +1,30 @@
+package dot;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.io.InputStream;
+import java.io.FileInputStream;
+
+import antlr4.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        String inputfile = null;
+        if(args.length > 0) inputfile = args[0];
+        if(inputfile != null){
+            DotParser parser = Visitor.parse(inputfile);
+            if(parser != null){
+                ParseTree tree = parser.graph();
+                System.out.println(tree.toStringTree(parser));
+            }
+        } 
+    }
+
+
+}
+
+
+
