6303dfb948b44fd189dc72cdb7658526cd1fe022
Ian A. Mason <iam@csl.sri.com>
2013-06-05 21:50:38 +0000

    Updating PLAOnline

--- src/iop_graphics2d_wrapper.c
+++ src/iop_graphics2d_wrapper.c
@@ -115,12 +115,12 @@ int main(int argc, char** argv){
   self_debug_flag  = G2D_ACTOR_DEBUG;
   self = argv[0];
 
-  //printArgv(stderr, argc, argv, "argv");   
-
+  /* printArgv(stderr, argc, argv, "argv");    */
+  
   g2dargc = make_arguments(argc, argv, &g2dargv);
-
-  //printArgv(stderr, g2dargc, g2dargv, "g2dargv");   
-
+  
+  /* printArgv(stderr, g2dargc, g2dargv, "g2dargv");  */
+  
   /*
     ec_null( g2dargv = calloc(argc + 4, sizeof(char*)) );
     g2dargv[0] = "java";
